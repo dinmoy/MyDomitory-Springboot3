28114395e63e7de6d933acf73745bf79c1a96cad
dinmoy <imhyojin614@gmail.com>
2024-05-07 01:38:50 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/jin/MyDomitory/MyDomitoryApplication.java
@@ -0,0 +1,12 @@
+package com.jin.MyDomitory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDomitoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDomitoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jin/MyDomitory/MyDomitoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.MyDomitory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDomitoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
