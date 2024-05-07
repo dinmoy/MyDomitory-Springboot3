7383595627f462dbc7351f672f601fa27a3a8bf5
dinmoy <imhyojin614@gmail.com>
2024-05-07 15:57:55 +0900

    feat : 세탁일지 엔티티 추가

---
+++ src/main/java/com/jin/MyDomitory/domain/Laundary.java
@@ -0,0 +1,28 @@
+package com.jin.MyDomitory.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+public class Laundary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int Id;
+
+    @Column(nullable = false,length = 15)
+    private String washer_num;
+
+    @Column(nullable = false,length = 15)
+    private String room_num;
+
+    @Builder
+    public Laundary(String washer_num,String room_num){
+        this.washer_num=washer_num;
+        this.room_num=room_num;
+    }
+
+}
+
+
