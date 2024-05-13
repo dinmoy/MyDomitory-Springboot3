0a91f5c0ce34a14aafc8f262862992adf609bb86
dinmoy <imhyojin614@gmail.com>
2024-05-13 22:40:52 +0900

    ci : spring security config 설정

---
+++ src/main/java/com/jin/MyDomitory/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.jin.MyDomitory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/user/signup", "/user/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(withDefaults());
+        return http.build();
+    }
+
+}
