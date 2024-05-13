14c0144c30c3fef99fd559c2d6eecf2c2c573fda
dinmoy <imhyojin614@gmail.com>
2024-05-13 13:35:59 +0900

    feat : 청소 도메인 추가

---
+++ src/main/java/com/jin/MyDomitory/domain/Cleaning.java
@@ -0,0 +1,32 @@
+package com.jin.MyDomitory.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+public class Cleaning {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(nullable = false)
+    private String roomNum;
+
+    @Column(nullable = false)
+    private String cleanPoint;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Builder
+    public Cleaning(String roomNum,String cleanPoint,LocalDate date){
+        this.roomNum=roomNum;
+        this.cleanPoint=cleanPoint;
+        this.date=date;
+    }
+}
